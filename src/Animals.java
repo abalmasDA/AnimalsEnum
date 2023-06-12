ac80f9d38e6c263f70aba08880781285621b0943
abalmasDA <abalmasit@gmail.com>
2023-06-12 21:47:13 +0300

    commit1

--- src/Animals.java
+++ src/Animals.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public enum Animals {
+import java.beans.beancontext.BeanContext;
+
+/**
+ * Створіть проект за допомогою IntelliJ IDEA. Створіть перерахунковий тип (enum) Animals, що містить конструктор,
+ * який повинен набувати цілого числа (вік тварини),
+ * і містити перевантажений метод toString(), який повинен повертати назву екземпляра та вік тварини.
+ */
+
+public enum Animals {
+    DOG(5),
+    CAT(10),
+    PIG(1),
+    FOX(7),
+    SNAKE(12),
+    BEAR(20);
+
+    private int ageAnimal;
+
+    Animals(int ageAnimal) {
+        this.ageAnimal = ageAnimal;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + super.toString() + ", " +
+                "age=" + ageAnimal +
+                '}';
+    }
 }
+
+
